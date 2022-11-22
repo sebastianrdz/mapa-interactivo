2cd7ff546be1ef4d5eb466ad8950e234f2997c65
sebastianrdz <74786081+sebastianrdz@users.noreply.github.com>
2022-11-22 11:48:14 -0600

    feat: map add on

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,15 +1,33 @@
 import styled from '@emotion/styled'
+import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
+import '../App.css'
 
 function InteractiveMap() {
+  const position = [51.505, -0.09]
   return (
-    <MapWrapper>InteractiveMap</MapWrapper>
+    <MapWrapper>
+      <MapContainer
+        center={position}
+        zoom={13}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            Hello!
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </MapWrapper>
   )
 }
 
-export default InteractiveMap
+export default InteractiveMap;
 
 const MapWrapper = styled.div`
   width: 100%;
   height: 400px;
-  border: 2px solid #000;
+  overflow: hidden;
 `
