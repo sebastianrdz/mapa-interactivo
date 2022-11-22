10b1fefb0e14360e19e6fe400882167c8246702f
Diego <A00827847@itesm.mx>
2022-11-22 11:56:16 -0600

    Added marker icon

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,6 +1,16 @@
 import styled from '@emotion/styled'
 import { MapContainer, TileLayer, Popup, Marker } from "react-leaflet";
 import '../App.css'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 function InteractiveMap() {
   const position = [51.505, -0.09]
