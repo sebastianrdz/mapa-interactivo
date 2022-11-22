250d768a2a20babf4dd6f73b8bb5a3a210325f0a
Sebastian Rdz <sebastian.rodriguez@umbrage.com>
2022-11-22 11:30:41 -0600

    limit map to div

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -5,9 +5,8 @@ import '../App.css'
 function InteractiveMap() {
   const defaultPosition = [48.864716, 2.349];
   return (
-    <div className='map-container'>
+    <MapWrapper>
       <MapContainer
-        className='leaflet-container'
         center={defaultPosition}
         zoom={13}
       >
@@ -16,7 +15,7 @@ function InteractiveMap() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </MapContainer>
-    </div>
+    </MapWrapper>
   )
 }
 
@@ -26,4 +25,5 @@ const MapWrapper = styled.div`
   width: 100%;
   height: 400px;
   border: 2px solid #000;
+  overflow: hidden;
 `
