b1ae3e5e04526c23cbf9f7702cce73600aec51aa
Diego <A00827847@itesm.mx>
2022-11-22 11:01:01 -0600

    added map, have to fix bug

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,12 +1,26 @@
 import styled from '@emotion/styled'
+import { MapContainer, TileLayer } from "react-leaflet";
+import '../App.css'
 
 function InteractiveMap() {
+  const defaultPosition = [48.864716, 2.349];
   return (
-    <MapWrapper>InteractiveMap</MapWrapper>
+    <div className='map-container'>
+      <MapContainer
+        className='leaflet-container'
+        center={defaultPosition}
+        zoom={13}
+      >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
   )
 }
 
-export default InteractiveMap
+export default InteractiveMap;
 
 const MapWrapper = styled.div`
   width: 100%;
