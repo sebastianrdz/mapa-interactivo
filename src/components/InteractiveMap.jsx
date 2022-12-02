f4f3c823c73561fc8fc1b6dc42845cf3b6e482e5
Diego <A00827847@itesm.mx>
2022-12-02 02:25:28 -0600

    cool ui

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -1,5 +1,4 @@
 import { MapContainer, TileLayer, Popup, Marker, Circle } from "react-leaflet";
-import { renderToStaticMarkup } from "react-dom/server";
 import icons from "leaflet-color-number-markers";
 import styled from "@emotion/styled";
 import "leaflet/dist/leaflet.css";
@@ -20,17 +19,8 @@ L.Icon.Default.mergeOptions({
 
 function InteractiveMap({ userPosition, info }) {
 	const [position] = useState([userPosition.latitude, userPosition.longitude]);
+
 	console.log(info)
-	const iconMarkup = renderToStaticMarkup(
-		<div>
-			Test
-		</div>
-	);
-
-	const customMarkerIcon = divIcon({
-		html: iconMarkup
-	});
-
 	return (
 		<MapWrapper>
 			<MapContainer center={position} zoom={13}>
@@ -56,14 +46,29 @@ function InteractiveMap({ userPosition, info }) {
 					info?.data?.data?.map((data) => {
 						var lat = data["latitud"];
 						var long = data["longitud"];
-						var nom_estab = data["nom_estab"];
+
+						var nom_estab = ((data["raz_social"] == null) ? data["nom_estab"] : data["raz_social"]);
 						var edificio = data["edificio"];
+						var ocupacion = data["per_ocu"];
+						var plaza = data["nom_CenCom"]
+						var localidad = data["localidad"]
+						var distancia = ((data["distancia"] == null) ? data["distancia"] : data["distancia"] + " km ");
+						var correo = data["correoelec"]
+
 						return (
 							<Marker position={[lat, long]} icon={icons.violet.numbers[0]}>
 								<Popup>
 									<ul>
-										<li>{nom_estab}</li>
+										<li className="title">{nom_estab}</li>
 										<li>{edificio}</li>
+										<li>{ocupacion}</li>
+										<li>{plaza}</li>
+										<li>{correo}</li>
+									</ul>
+									<div className="divide"></div>
+									<ul>
+										<li>{distancia}</li>
+										<li>{localidad}, Nuevo León</li>
 									</ul>
 								</Popup>
 							</Marker>
@@ -77,12 +82,6 @@ function InteractiveMap({ userPosition, info }) {
 export default InteractiveMap;
 
 const MapWrapper = styled.div`
-	width: 100%;
-	height: 400px;
-	overflow: hidden;
-`;
-
-const StarPoint = styled.div`
 	width: 100%;
 	height: 400px;
 	overflow: hidden;
